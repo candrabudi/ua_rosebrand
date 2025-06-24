72d19176426378bbb3411f55988f8c2a60b4e8fa
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-06-24 14:22:34 +0700

    fix home

--- app/Http/Controllers/UA/UAHomeController.php
+++ app/Http/Controllers/UA/UAHomeController.php
@@ -14,7 +14,7 @@ class UAHomeController extends Controller
     {
         $topRosebrandProducts = Product::select('products.*', DB::raw('SUM(order_items.quantity) as total_sold'))
             ->join('order_items', 'products.id', '=', 'order_items.product_id')
-            ->groupBy('products.id', 'products.category_id', 'products.name')
+            ->groupBy('products.id', 'products.category_id', 'products.name', 'products.slug')
             ->orderByDesc('total_sold')
             ->take(8)
             ->get();
